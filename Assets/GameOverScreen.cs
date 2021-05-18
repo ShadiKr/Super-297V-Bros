d26960987ceb21f5787b16ba83cc7aa56a8ad15a
mgn17 <49205277+mgn17@users.noreply.github.com>
2021-05-18 15:54:28 +0300

    Add files via upload

---
+++ Assets/GameOverScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public Text pointsText;
+    // Start is called before the first frame update
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        pointsText.text = "Your score is:" + score.ToString() + "Points";
+        
+    }
+}
